35dac40d9c9f8b006c542d7663608bbccd7fcd27
Juan Antonio Ortiz Guerra <juanantonioortizguerra@gmail.com>
2023-11-02 12:19:58 +0100

    added first animation character

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -8,26 +8,38 @@ public class AgentScript : MonoBehaviour
     public List<Transform> waypoints; // Lista de los objetos que representan los waypoints
     private int currentWaypointIndex = 0; // Índice del waypoint actual
     private NavMeshAgent agent; // Componente NavMeshAgent del agente
+     private float minSpeed = 5f; // Velocidad mínima del agente
+    private float maxSpeed = 15f; // Velocidad máxima del agente
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>(); // Obtener el componente NavMeshAgent del agente
-        SetDestination(); // Establecer el destino inicial
+        agent = GetComponent<NavMeshAgent>(); 
+        SetDestination();
+        agent.speed = Random.Range(minSpeed, maxSpeed); 
+        SetRandomRotation();
     }
 
     void SetDestination()
     {
-        if (waypoints.Count == 0) return; // Si no hay waypoints, salir
-        agent.SetDestination(waypoints[currentWaypointIndex].position); // Establecer el destino al waypoint actual
+        if (waypoints.Count == 0) return; 
+        agent.SetDestination(waypoints[currentWaypointIndex].position); 
     }
 
     void Update()
     {
-        // Si el agente está cerca del waypoint actual, pasar al siguiente
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
         {
-            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Count; // Avanzar al siguiente waypoint
-            SetDestination(); // Establecer el nuevo destino
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Count; 
+            agent.speed = Random.Range(minSpeed, maxSpeed);
+            Debug.Log("velocidad = "+agent.speed);
+            SetDestination(); 
+            SetRandomRotation();
         }
+    }
+     void SetRandomRotation()
+    {
+        float randomYRotation = Random.Range(0, 360); // Generar un ángulo aleatorio en el eje Y (vertical)
+        transform.rotation = Quaternion.Euler(50, randomYRotation, 40); // Aplicar la rotación aleatoria al agente
+        Debug.Log("rotacion = "+transform.rotation);
     }
 }
